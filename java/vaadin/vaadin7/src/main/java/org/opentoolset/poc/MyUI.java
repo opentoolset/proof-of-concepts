7f874650c6eadef679dbae7d23653e71bf917c44
hadidilek <hadi.dilek@gmail.com>
2021-02-01 08:01:26 +0300

    diagram builder removed

--- java/vaadin/vaadin7/src/main/java/org/opentoolset/poc/MyUI.java
+++ java/vaadin/vaadin7/src/main/java/org/opentoolset/poc/MyUI.java
@@ -1,17 +1,5 @@
 package org.opentoolset.poc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.vaadin.diagrambuilder.DiagramBuilder;
-import org.vaadin.diagrambuilder.DiagramBuilder.StateCallback;
-import org.vaadin.diagrambuilder.DiagramStateEvent;
-import org.vaadin.diagrambuilder.Node;
-import org.vaadin.diagrambuilder.NodeType;
-import org.vaadin.diagrambuilder.Transition;
-
-import com.vaadin.annotations.JavaScript;
-import com.vaadin.annotations.StyleSheet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
@@ -27,8 +15,6 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
-@JavaScript("http://cdn.alloyui.com/2.5.0/aui/aui-min.js")
-@StyleSheet("http://cdn.alloyui.com/2.5.0/aui-css/css/bootstrap.min.css")
 @SpringUI
 public class MyUI extends UI {
 
@@ -41,39 +27,12 @@ public class MyUI extends UI {
 		final TextField name = new TextField();
 		name.setCaption("Type your name here:");
 
-		DiagramBuilder diagramBuilder = new DiagramBuilder();
-
 		Button button = new Button("Click Me");
 		button.addClickListener(e -> {
 			layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
-
-			diagramBuilder.getDiagramState(new StateCallback() {
-
-				@Override
-				public void onStateReceived(DiagramStateEvent event) {
-					List<Node> nodes = event.getNodes();
-					System.out.println(nodes);
-				}
-			});
 		});
 
-		List<NodeType> nodeTypes = new ArrayList<>();
-		nodeTypes.add(new NodeType("diagram-node-start-icon", "Start", "start"));
-		nodeTypes.add(new NodeType("diagram-node-fork-icon", "Fork", "fork"));
-		nodeTypes.add(new NodeType("diagram-node-condition-icon", "Condition", "condition"));
-
-		List<Node> nodes = new ArrayList<>();
-		nodes.add(new Node("StartNode", "start", 10, 10));
-		nodes.add(new Node("Condition", "condition", 260, 16));
-
-		diagramBuilder.setAvailableFields(nodeTypes.toArray(new NodeType[] { }));
-		diagramBuilder.setFields(nodes.toArray(new Node[] { }));
-
-		diagramBuilder.setTransitions(new Transition("StartNode", "Condition", "TaskConnector1"));
-		diagramBuilder.setSizeFull();
-
-		layout.addComponents(name, button, diagramBuilder);
-		layout.setExpandRatio(diagramBuilder, 1);
+		layout.addComponents(name, button);
 		layout.setMargin(true);
 		layout.setSpacing(true);
 
